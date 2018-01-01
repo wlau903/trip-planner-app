85b6d5e3491a22fe192055b191ef96e15c576ca1
Wendy Lau <wlau903@gmail.com>
2018-01-01 20:33:22 +0000

    create users and trips dbs

---
+++ db/migrate/20180101202208_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration
+  def change
+    create_table :trips do |t|
+      t.string :name
+      t.date :date
+      t.string :destination
+      t.text :activities
+      t.integer :user_id
+    end
+  end
+  
+end
