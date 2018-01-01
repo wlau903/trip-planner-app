b0a4e59f359b2d180d3697da9bdc5fb620627a4a
Wendy Lau <wlau903@gmail.com>
2018-01-01 22:43:50 +0000

    set home page and mount controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "travel"
   end
 
-  get "/" do
-    
+  get '/' do
+    erb :index
   end
 
 end
