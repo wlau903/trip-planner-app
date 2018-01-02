690ff89e01bb911de6504fc800e2b3d43244b6e2
Wendy Lau <wlau903@gmail.com>
2018-01-02 05:43:08 +0000

    get, post /login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,22 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect("/trips")
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect("/trips")
+    else
+      redirect("/signup")
+    end
+  end
+
 end
