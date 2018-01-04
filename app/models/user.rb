34661be8341c96aca21d0a0f7261f9e95e23bcce
Wendy Lau <wlau903@gmail.com>
2018-01-04 01:13:57 +0000

    add username slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many :trips
   has_secure_password
+
+  def slug
+    self.username.downcase.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |instance| instance.slug == slug }
+  end
+
 end
